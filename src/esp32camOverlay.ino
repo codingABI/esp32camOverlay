9ebcb94e522ad36ae6cd562d1e8e0c9d506d6e3d
codingABI <50620368+codingABI@users.noreply.github.com>
2023-03-11 21:16:47 +0100

    Update esp32camOverlay.ino

--- src/esp32camOverlay.ino
+++ src/esp32camOverlay.ino
@@ -1,6 +1,6 @@
 /*
  * Project: esp32camOverlay
- * Description: Example sketch to show, how to draw text or lines on a esp32-cam captured photo and save the picture on a local SD card
+ * Description: Example sketch to show, how to draw text or lines on a esp32-cam captured photo and save the picture on a SD card
  * 
  * This could be useful for example, if want to show a timestamp, logo oder label on a photo.
  * Due memory limitations no resolution higher then XGA (1024x768) is possible with 4 MB psram 
